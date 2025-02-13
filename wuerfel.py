8d39e0923d0883925c30d477cea9286340dd584a
pavetinari <naumann.henning@aol.de>
2025-02-13 16:07:10 +0100

    ja yeet

---
+++ wuerfel.py
@@ -0,0 +1,18 @@
+import random
+
+
+class Wuerfel:
+
+    """Diese initialisiert."""
+
+    def __init__(self, seiten_anzahl: int, wuerfel_name: int):
+        self.seiten_anzahl = seiten_anzahl
+        self.wuerfel_name = wuerfel_name
+
+    def wuerfeln(self):
+        """
+        Die Methode wählt eine zufällige Zahl im bereich zwischen 1 und
+        der Seitenanzahl des Würfels aus.
+        """
+
+        return random.randint(1, self.seiten_anzahl)
