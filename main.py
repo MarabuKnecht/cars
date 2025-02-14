a94be213e367fcc344fc5ba40aab0194a13fd42b
schlusi <silasschlueter@gmx.de>
2025-02-14 14:25:43 +0100

    hiernist die rennen klasse mitsamt rennen methode

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from model.rennen import Rennen
 def main():
 
 # Silas 1, Sinan 2, Hennin 3, Eduard 4, Jalal 5, Zakariya 6, Joel 7
-    wagen1 = Rennwagen()
+    wagen1 = Rennwagen("silas super schlieten",4,4,0)
     wagen2 = Rennwagen()
     wagen3 = Rennwagen()
     wagen4 = Rennwagen()
