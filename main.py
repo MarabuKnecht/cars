424b96a9e34389cfb0ef01e9a8d00b0c56ed5ec4
MarabuKnecht <poeljaletta@gmail.com>
2025-02-14 14:16:36 +0100

    main erstellt mit platzhaltern für rennwagen

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hey Hey People")
+from model.rennwagen import Rennwagen
+from model.rennen import Rennen
+
+
+def main():
+
+    wagen1 = Rennwagen()
+    wagen2 = Rennwagen()
+    wagen3 = Rennwagen()
+    wagen4 = Rennwagen()
+    wagen5 = Rennwagen()
+    wagen6 = Rennwagen()
+
+    rennen = Rennen()
+    rennen.startetrennen(wagen1,wagen2,wagen2,wagen3,wagen4,wagen5,wagen6)
